cd0f964a2b70b5b5bf3aa049e1dbeedb51258db0
PillouMK <maxence.pille@gmail.com>
2022-09-05 03:37:27 +0200

    updated and optimized /player GETs routes, deleted class models

--- controller/playerController.js
+++ controller/playerController.js
@@ -1,39 +1,70 @@
 const express = require('express');
 const router = express.Router();
 const config = require('../databaseConfig.js');
-const Player = require('../models/playerModel');
-const Timetrial = require('../models/timetrialModel.js');
 
 // Connexion à la database
 const db = config.connection;
 
 
 function getAllPlayers(res) {
-    const arrayPlayer = [];
-    db.query("SELECT * FROM player", function(err, result) {
+    db.query("SELECT *, r.rosterName FROM player p join roster r on p.idRoster = r.idRoster", function(err, result) {
         
-        result.forEach(element => {
-            let player = new Player();
-            player = player.fromJSON(element);
-            arrayPlayer.push(player);
-        });
+        if(Array.isArray(result) && result.length) {
+            // sort array (usefull for timetrial ranking, no impact for the rest)
+            result.sort((a, b) => {
+                if(b.tt_points > a.tt_points) {
+                    return 1;
+                }
+                if(b.tt_points < a.tt_points) {
+                    return -1
+                }
+                if(b.tt_top1 > a.tt_top1) {
+                    return 1;
+                }
+                if(b.tt_top1 < a.tt_top1) {
+                    return -1
+                }
+                if(b.tt_top3 > a.tt_top3) {
+                    return 1;
+                }
+                else {
+                    return -1
+                }
 
-        // tri du tableau
-        // arrayPlayer.sort((a, b) => b.tt_points - a.tt_points);
-        res.send(arrayPlayer)  
+            });
+
+            // send the response
+            res.send({
+                responseCode : 1,
+                response : result
+            });
+        } else {
+            // send the response
+            res.send({
+                responseCode : 0,
+                response : "There is no players"
+            });
+        }
     });
 }
 
 
 function getPlayer(req, res) {
-    db.query("SELECT * FROM player WHERE player.idPlayer = "+req.params.idPlayer, function(err, result) {
+    db.query("SELECT p.*, r.rosterName FROM player p join roster r on p.idRoster = r.idRoster WHERE p.idPlayer = "+req.params.idPlayer, function(err, result) {
+        // check if player exist
         if(Array.isArray(result) && result.length) {
-            let player = new Player();
-            player = player.fromJSON(result[0]);
-            res.send(player);
+                // send the response
+                res.send({
+                    responseCode : 1,
+                    response : {
+                        player : result[0]
+                    }
+                });
         } else {
+            // if no data player don't exist
             res.send({
-                response : "idPlayer n'existe pas"
+                responseCode : 0,
+                response : `${req.params.idPlayer} n'existe pas`
             });
         }
         
@@ -45,31 +76,31 @@ function getTimetrialFromPlayer(req, res) {
     const arrayTimetrial = [];
     const arrayShroom = [];
     const arrayNoShroom = [];
-    db.query(`SELECT * FROM player where player.idPlayer = ${req.params.idPlayer};SELECT *, m.nameMap FROM timetrial as tm JOIN map as m on m.id=tm.idMap WHERE tm.idPlayer = ${req.params.idPlayer};`, (err, result) => {
+    db.query(`SELECT * FROM player where player.idPlayer = ${req.params.idPlayer};SELECT tm.idMap, m.nameMap, tm.time, tm.date FROM timetrial as tm JOIN map as m on m.id=tm.idMap WHERE tm.idPlayer = ${req.params.idPlayer};`, (err, result) => {
 
         // player exist
         if(Array.isArray(result[0]) && result[0].length) {
 
-            // create player variable
-            let player = new Player();
-            player = player.fromJSON(result[0][0]);
-            console.log("player", player);
-            
+            // player variable  
+            const player = result[0][0];
+           
+
             // player have timetrials
             if(Array.isArray(result[1]) && result[1].length) {
                 result[1].forEach(element => {
-                    let timetrial = new Timetrial();
-                    timetrial = timetrial.fromJSON(element);
-                    delete timetrial.idPlayer;
-                    timetrial.nameMap = element.nameMap;
-                    if(element.isShroomless == "0") {
+                    let timetrial = element;
+                    
+
+                    // Sort timetrials by isShroomless
+                    if(!timetrial.isShroomless) {
                         arrayShroom.push(timetrial)
                     } else {
                         arrayNoShroom.push(timetrial);
                     }
+                    delete timetrial.isShroomless
                 });
-                arrayTimetrial.push({"arrayShroom" : arrayShroom});
-                arrayTimetrial.push({"arrayNoShroom" : arrayNoShroom});
+                arrayTimetrial.push({"arrayShroom" : arrayShroom.length ? arrayShroom : null});
+                arrayTimetrial.push({"arrayShroomless" : arrayNoShroom.length ? arrayNoShroom : null});
                 res.send({
                     responseCode : 1,
                     response : {
